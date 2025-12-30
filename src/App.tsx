9d2717d563c003bf58b55661520da4c67d792983
김태훈 <je64768035@gmail.com>
2025-12-30 10:01:12 +0900

    fix : vercel build 오류 해결 후 App.tsx 원복

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-
+import { Outlet } from 'react-router-dom';
+import { HeaderLayout } from '@/components/layout/header/HeaderLayout';
+import { RootLayout } from '@/components/layout/rootLayout/RootLayout';
+import styles from '@/App.module.scss'
 
 function App() {
   return (
-    <div >
-     test
+    <div className={styles.app}>
+      <HeaderLayout />
+      <div className={styles.contents}>
+        <Outlet />
+      </div>
+      <RootLayout />
     </div>
   )
 }
