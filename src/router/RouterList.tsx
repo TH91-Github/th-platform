5dbfd186cdc32e14efc3a90416c720c704ad4762
김태훈 <je64768035@gmail.com>
2025-12-30 10:43:11 +0900

    chore : router 경로 수정 2

--- src/router/RouterList.tsx
+++ src/router/RouterList.tsx
@@ -1,7 +1,11 @@
 
 // 🔹 Pages
 
+import { AboutPage } from "@/pages/about/AboutPage";
+import { ContactPage } from "@/pages/contact/ContactPage";
+import { DivisionPage } from "@/pages/division/DivisionPage";
 import { GuidePage } from "@/pages/guide/GuidePage";
+import { PortfolioPage } from "@/pages/portfolio/PortfolioPage";
 import { GUIDE_LIST } from "./guide/GuideLists";
 
 export const routerList = [
