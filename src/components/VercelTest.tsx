0358ef17a3feef47ab47c75162906d9b2f584054
김태훈 <je64768035@gmail.com>
2025-12-30 10:41:09 +0900

    chore : router 경로 수정

--- src/components/VercelTest.tsx
+++
@@ -1,8 +0,0 @@
-
-export const VercelTest = () => {
-  return ( 
-    <div>
-      VercelTest
-    </div>
-  )
-}
--- src/router/RouterList.tsx
+++ src/router/RouterList.tsx
@@ -1,60 +1,17 @@
 
 // 🔹 Pages
 
-import { AboutPage } from "@/pages/about/AboutPage";
-import { ContactPage } from "@/pages/contact/ContactPage";
-import { DivisionPage } from "@/pages/division/DivisionPage";
 import { GuidePage } from "@/pages/guide/GuidePage";
-import { PortfolioPage } from "@/pages/portfolio/PortfolioPage";
 import { GUIDE_LIST } from "./guide/GuideLists";
 
 export const routerList = [
-  { //  개발 전용
+  { 
     id:'guide',
     path: '/guide',
     title:'Guide',
-    view: 'dev', // dev모드
+    // view: 'dev', // dev모드
     element: <GuidePage />,
     children: [...GUIDE_LIST],
-  },
-  {
-    id:'about',
-    path: '/about',
-    title:'About',
-    element: <AboutPage />,
-    children:[
-      // about
-      // ceo
-      //contact us
-    ]
-  },
-  {
-    id:'portfolio',
-    path: '/portfolio',
-    title:'Portfolio',
-    element: <PortfolioPage />,
-    children:[
-      // 통합 or 부문별 작업 
-    ]
-  },
-  {
-    id:'division',
-    path: '/division',
-    title:'Division',
-    element: <DivisionPage />,
-    children:[
-      // 1 or 하위
-    ]
-    //Division 상위 / Department 하위
-  },
-  {
-    id:'contact',
-    path: '/contact',
-    title:'Contact',
-    element: <ContactPage />,
-    children:[
-      // 1 or 하위
-    ]
   },
 ];
 
