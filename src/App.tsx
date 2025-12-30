81470423accdfc7b6cd7ece5144cb3a21a5f7cc1
김태훈 <je64768035@gmail.com>
2025-12-30 10:18:54 +0900

    fix : vercel build 오류 해결 시도 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import styles from '@/App.module.scss'
 import { VercelTest } from '@/components/VercelTest'
+import { Header } from './components/layout/header/Header'
 
 function App() {
   return (
     <div className={styles.app}>
+      <Header />
      <VercelTest />
     </div>
   )
---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,12 @@
+
+import styles from './Header.module.scss';
+export const Header = () => {
+  
+  return (
+    <div className={styles.header}>
+      <div className={`cont-inner ${styles['header-inner']}`}>
+      
+      </div>
+    </div>
+  )
+}
