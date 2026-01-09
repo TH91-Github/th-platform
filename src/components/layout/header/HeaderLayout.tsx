5017d38c30c4f921ba2683ab4006d8fa4f4ac681
김태훈 <je64768035@gmail.com>
2026-01-09 15:18:21 +0900

    feat: header > logo, gnb

--- src/components/layout/header/HeaderLayout.tsx
+++ src/components/layout/header/HeaderLayout.tsx
@@ -1,11 +1,23 @@
 
+import { cn } from '@/utils/common';
 import styles from './HeaderLayout.module.scss';
+import { useToggle } from '@/hook/common/useToggle';
+import { LogoIcon } from '@/components/ui/icon/LogoIcon';
+import { GnbMenu } from './gnbMenu/GnbMenu';
+import { ToolMenu } from './toolMenu/ToolMenu';
 
 export const HeaderLayout = () => {
-  
+  const [isFull, useIsFull] = useToggle(true);
+
   return (
     <div className={styles.header}>
-    
+      <div className={styles.header}>
+        <div className={cn(isFull ? styles.full : 'cont-inner', styles.inner)}>
+          <LogoIcon />
+          <GnbMenu />
+          <ToolMenu />
+        </div>
+      </div>
     </div>
   )
 }
