d1ab75211ba18c11858c4ddc80a32c94f0f00a75
김태훈 <je64768035@gmail.com>
2026-01-18 21:27:44 +0900

    chore: 더미 데이터 및 main 반영

---
+++ src/data/hub/hubData.ts
@@ -0,0 +1,135 @@
+import type { HubCategoryDataType, HubTotalType } from "@/types/hub/hub";
+
+// 🔹 default 카테고리 ko, en 정보
+export const hubCategoryData: HubCategoryDataType[] = [
+  {
+    id:'normal',
+    title:'일반',
+  },
+  {
+    id:'travel',
+    title:'여행',
+  },
+  {
+    id:'calendar',
+    title:'달력',
+  },
+  {
+    id:'memo',
+    title:'메모',
+  },
+  {
+    id:'cashledger',
+    title:'가계부',
+  },
+  {
+    id:'running',
+    title:'러닝',
+  },
+]
+// 🔹 hub 카테고리 total default
+export const hubTotalData : HubTotalType[] = [
+  {
+    totalTitle:'전체',
+    totalCategory:'total',
+    totalLists:[
+      {
+        id:'all',
+        title:'전체',
+        desc:'개설/참여하고 있는 모든 방을 안내하고 있어요.',
+        total:0,
+      },
+      {
+        id:'single',
+        title:'나만의 방',
+        desc:'1인 구성으로 이루어진 방을 안내하고 있어요.',
+        total:0,
+      },
+      {
+        id:'multi',
+        title:'함께',
+        desc:'2인 이상 팀을 이루어진 방을 안내하고 있어요.',
+        total:0,
+      },
+      {
+        id:'public',
+        title:'공개',
+        desc:'공개로 되어 있는 방을 안내하고 있어요.',
+        total:0,
+      },
+      {
+        id:'private',
+        title:'비공개',
+        desc:'비공개로 되어 있는 방을 확인할 수 있어요.',
+        total:0,
+      }
+    ],
+  },
+  {
+    totalTitle:'카테고리',
+    totalCategory:'category',
+    totalLists:[
+      {
+        id:'normal',
+        title:'일반',
+        desc:'자유롭게 이용하는 방을 안내하고 있어요',
+        total:0,
+      },
+      {
+        id:'travel',
+        title:'여행',
+        desc:'가족, 친구들과 여행 일정, 정산 등 확인하고 초대해보세요.',
+        total:0,
+      },
+      {
+        id:'calendar',
+        title:'일정',
+        desc:'일정을 등록하고 일정을 공유해보세요.',
+        total:0,
+      },
+      {
+        id:'memo',
+        title:'메모',
+        desc:'메모, 체크리스트 등을 기록하고 사용하세요.',
+        total:0,
+      },
+      {
+        id:'cashledger',
+        title:'가계부',
+        desc:'지출, 수입, 투자 등 꼼꼼하게 기록하고 확인하세요.',
+        total:0,
+      },
+      {
+        id:'running',
+        title:'러닝',
+        desc:'매일 러닝 하고 기록하고 공유해보세요.',
+        total:0,
+      },
+    ]
+  },
+  {
+    totalTitle:'즐겨찾기',
+    totalCategory:'bookmark',
+    totalLists:[
+      {
+        id:'all',
+        title:'모든 즐겨찾기',
+        desc:'즐겨찾기 되어 있는 방을 확인할 수 있어요.',
+        total:0,
+      },
+      {
+        id:'category',
+        title:'카테고리별 즐겨찾기',
+        desc:'즐겨찾기 일반 즐겨찾기 방 수',
+        total:0,
+      },
+      {
+        id:'public',
+        title:'공개/비공개 즐겨찾기',
+        desc:'즐겨찾기 일반 즐겨찾기 방 수',
+        total:0,
+      },
+    ]
+  },
+  
+];
--- src/router/RouterList.tsx
+++ src/router/RouterList.tsx
@@ -1,16 +1,10 @@
 
 // 🔹 Pages
 
-import { AboutPage } from "@/pages/about/AboutPage";
-import { ContactPage } from "@/pages/contact/ContactPage";
-import { DivisionPage } from "@/pages/division/DivisionPage";
-import { GuidePage } from "@/pages/guide/GuidePage";
-import { PortfolioPage } from "@/pages/portfolio/PortfolioPage";
-import { GUIDE_LIST } from "./guide/GuideLists";
-import { RunPage } from "@/pages/run/RunPage";
-import { HubPage } from "@/pages/hub/HubPage";
 import { HubMain } from "@/components/pages/hub/HubMain";
-
+import { GuidePage } from "@/pages/guide/GuidePage";
+import { HubPage } from "@/pages/hub/HubPage";
+import { GUIDE_LIST } from "./guide/GuideLists";
 export const routerList = [
   {
     id: 'guide',
@@ -21,24 +15,24 @@ export const routerList = [
     },
     children: [...GUIDE_LIST],
   },
-  {
-    id: 'hub',
-    path: '/hub',
-    element: <HubPage />,
-    handle: {
-      title: 'Hub',
-    },
-    children: [
-      {
-        id: 'hub-main',
-        index:true,
-        element: <HubMain />,
-        handle: {
-          title: 'Hub',
-        },
-      },
-    ]
-  },
+  // {
+  //   id: 'hub',
+  //   path: '/hub',
+  //   element: <HubPage />,
+  //   handle: {
+  //     title: 'Hub',
+  //   },
+  //   children: [
+  //     {
+  //       id: 'hub-main',
+  //       index:true,
+  //       element: <HubMain />,
+  //       handle: {
+  //         title: 'Hub',
+  //       },
+  //     },
+  //   ]
+  // },
   // {
   //   id: 'run',
   //   path: 'run',
