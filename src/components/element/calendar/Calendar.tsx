e5609849c65845b7e0baf9455fb84f640fed8e12
김태훈 <je64768035@gmail.com>
2026-01-18 21:22:42 +0900

    feat: calendar 컴포넌트 1차 추가

---
+++ src/components/element/calendar/Calendar.tsx
@@ -0,0 +1,65 @@
+import type { CalendarBasePropsType } from '@/types/date/calendar';
+import { useRef, useState } from 'react';
+import styles from './Calendar.module.scss';
+import { DateGrid } from './DateGrid';
+import { Week } from './Week';
+import { cn } from '@/utils/common';
+
+// 🔹 Calendar
+interface CalendarTPropsType extends CalendarBasePropsType {
+  className?:string,
+  weekClassName?: string,
+  daysClassName?:string,
+}
+
+export const Calendar = ({
+  selectDates,
+  events,
+  isHolidays,
+  isReadonly,
+  selectOne,
+  className,
+  weekClassName,
+  daysClassName,
+  onChange,
+  dayRender,
+}:CalendarTPropsType) => {
+  const calendarRef = useRef<HTMLDivElement>(null);
+  const today = new Date();
+  const [year, setYear] = useState(today.getFullYear());
+  const [month, setMonth] = useState(today.getMonth());
+
+  const moveMonth = (diff: number) => {
+    const date = new Date(year, month + diff, 1);
+    setYear(date.getFullYear());
+    setMonth(date.getMonth());
+  };
+
+  return(
+    <div 
+      ref={calendarRef} 
+      className={cn(styles.calendar, className)}
+    >
+      <div className={styles.heading}>
+        <button onClick={() => moveMonth(-1)}>◀</button>
+        <span>
+          {year}.{month + 1}
+        </span>
+       <button onClick={() => moveMonth(1)}>▶</button>
+      </div>
+      <Week className={weekClassName} />
+      <DateGrid
+        y={year} 
+        m={month}
+        selectDates={selectDates}
+        events={events}
+        isHolidays={isHolidays}
+        selectOne={selectOne}
+        isReadonly={isReadonly}
+        dayRender={dayRender}
+        className={daysClassName}
+        onChange={onChange}
+      />
+    </div>
+  )
+}
