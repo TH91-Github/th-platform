d75ec83a4c8df972a53409f813b997651e490973
김태훈 <je64768035@gmail.com>
2026-01-27 14:02:45 +0900

    chore: 오타 수정

--- src/components/pages/main/GuideLayout.tsx
+++ src/components/pages/main/GuideLayout.tsx
@@ -20,7 +20,7 @@ export const GuideLayout = () => {
         <div className={styles.heading}>
           <h3 className={styles.title}>개발 가이드</h3>
           <p className={styles.desc}>
-            관된 개발 경험을 위한 디자인 시스템과 코드 가이드.<br />
+            연관된 개발 경험을 위한 디자인 시스템과 코드 가이드.<br />
             색상, 타이포그래피부터 재사용 가능한 <br className="mo-only" />컴포넌트와 유틸리티까지 <br />
             효율적이고 일관된 개발을 위한 통합 가이드
           </p>
