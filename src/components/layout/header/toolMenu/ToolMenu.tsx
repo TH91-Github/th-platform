e2871d3c5f6ed4469f1de9d871186f570d08f622
김태훈 <je64768035@gmail.com>
2026-02-05 08:48:40 +0900

    chore : 로그인 버튼 재노출

--- src/components/layout/header/toolMenu/ToolMenu.tsx
+++ src/components/layout/header/toolMenu/ToolMenu.tsx
@@ -1,6 +1,6 @@
 
 import { ThemeToggle } from '@/components/common/theme/ThemeToggle';
-// import { LoginBtn } from '@/components/common/auth/LoginBtn';
+import { LoginBtn } from '@/components/common/auth/LoginBtn';
 import styles from './ToolMenu.module.scss';
 
 export const ToolMenu = () => {
@@ -8,7 +8,7 @@ export const ToolMenu = () => {
   return (
     <div className={styles.toolMenu}>
       {/* 로그인 */}
-      {/* <LoginBtn /> */}
+      <LoginBtn />
       {/* 다크/라이트 */}
       <ThemeToggle />
     </div>
