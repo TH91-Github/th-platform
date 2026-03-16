cb273cec10ce1fd9922d27905a68716765f899ea
김태훈 <je64768035@gmail.com>
2026-03-16 14:33:39 +0900

    feat: Hub 안내 모달 추가

--- src/pages/hub/HubPage.tsx
+++ src/pages/hub/HubPage.tsx
@@ -1,10 +1,22 @@
 import { Outlet } from 'react-router-dom';
 import styles from './HubPage.module.scss';
 import { HubMenu } from '@/components/pages/hub/HubMenu';
+import { Modal } from '@/components/element/modal/Modal';
 
 export const HubPage = () => {
+  
+  const onClose = () => {
+
+  }
   return ( 
     <div className={styles.hub}>
+      <Modal onClose={onClose}>
+        <div>
+          "현재 개발 중입니다."<br />
+          멋진 결과물로 찾아뵙겠습니다.<br />
+          🙇‍♂️
+        </div>
+      </Modal>
       {/* hub side */}
       <div className={styles.side}>
         <div className={styles.sideInner}>
